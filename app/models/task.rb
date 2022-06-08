a889205367bb500836de8224686bfc468b347589
junho jang <cclemon3248@gmail.com>
2022-06-08 13:20:04 +0900

    model設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :image, presence: true
   belongs_to :user
+  has_many :likes, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_07_102606) do
+ActiveRecord::Schema.define(version: 2022_06_08_024215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "task_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_likes_on_task_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "store"
@@ -47,5 +56,7 @@ ActiveRecord::Schema.define(version: 2022_06_07_102606) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "tasks"
+  add_foreign_key "likes", "users"
   add_foreign_key "tasks", "users"
 end
