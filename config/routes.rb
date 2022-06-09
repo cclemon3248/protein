1e7539058fa5c913f314a495d76768a48cefe4fc
junho jang <cclemon3248@gmail.com>
2022-06-10 01:37:38 +0900

    アカウントのリンク

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+  get 'users/show'
   resources :tasks
   root 'tasks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
