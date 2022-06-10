66f8449df2a796d6349844d605f19d3198d20ab9
junho jang <cclemon3248@gmail.com>
2022-06-11 01:42:09 +0900

    commentのモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_09_152003) do
+ActiveRecord::Schema.define(version: 2022_06_10_164135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "task_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_comments_on_task_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -65,6 +75,8 @@ ActiveRecord::Schema.define(version: 2022_06_09_152003) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "tasks"
+  add_foreign_key "comments", "users"
   add_foreign_key "favorites", "tasks"
   add_foreign_key "favorites", "users"
   add_foreign_key "likes", "tasks"
