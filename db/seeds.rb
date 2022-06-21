799a55be8dacf97e29be468f4f504360617360b7
junho jang <cclemon3248@gmail.com>
2022-06-22 02:10:50 +0900

    6

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 Task.create!(user_id: 1, store: "セブンイレブン", name:"たんぱく質が摂れる豚レバー", protein: 39.9, calorie: 371, price: 343.44, 
-image: File.open("./public/seven/7001.jpg", "r"))
+image: File.open("./public/seven/7001.jpg"))
