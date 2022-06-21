d89338df957f61e9b393699a03c7a7b0f757850c
junho jang <cclemon3248@gmail.com>
2022-06-22 04:10:38 +0900

    1

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,3 @@ Task.create!(
   price: 430, 
   image: File.open("./public/lawson/6001.png")
 )
-
-Task.create!(
-  user_id: 1, 
-  store: "ローソン", 
-  name: "ねぎ塩豚カルビ弁当(もち麦入りご飯)", 
-  protein: 21.6, 
-  calorie: 586, 
-  price: 430, 
-  image: File.open("user.jpg")
-)
