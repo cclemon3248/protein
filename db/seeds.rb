9999fccc8776d8a9793f445fdc3bb8ea1868b32c
junho jang <cclemon3248@gmail.com>
2022-06-22 17:13:13 +0900

    test3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 Task.create!(
-  user_id: 1, 
-  store: "セブンイレブン", 
-  name:"たんぱく質が摂れる豚レバー", 
-  protein: 39.9, 
-  calorie: 371, 
-  price: 343.44, 
-  image: File.open("./public/seven/7001.jpeg")
+  user_id:1, 
+  store: "ファミリーマート", 
+  name: "直火焼チャーシュー丼", 
+  protein: 15.4, 
+  calorie: 476, 
+  price: 356, 
+  image: File.open("./public/family/5001.jpeg")
 )
