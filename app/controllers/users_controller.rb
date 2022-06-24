7b6f48eb5e1c73f45d4acb1f610dc412805dd45d
junho jang <cclemon3248@gmail.com>
2022-06-24 11:56:26 +0900

    コメントアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @favorite_id = current_user.id
     @favorites = Favorite.where(user_id: @favorite_id)
-    
   end
 
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_user_session_path
+  end
 end
