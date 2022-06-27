5bd7bf383ffe811f76a9b7d9e2ee9e42147a6be3
junho jang <cclemon3248@gmail.com>
2022-06-27 15:35:33 +0900

    rails db:migrate:reset RAILS_ENV=test

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    name { 'お肉' }
+    email { "cclemon3248@gmail.com" }
+    password { "s5mYmhCy" }
+  end
+end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,31 +1,59 @@
 require 'rails_helper'
 RSpec.describe 'テスト', type: :system do
-  
   describe 'タスクのCRUD機能テスト' do
+    task1 = FactoryBot.create(:task)
 
     it 'タスク作成' do
       visit new_user_session_path
       click_link 'ゲストログイン'
       visit new_task_path
       select "ローソン", from: "task_store"
-      fill_in "商品名", with: 1
+      fill_in "商品名", with: "おじさん"
       fill_in "タンパク質量", with: 1
       fill_in "カロリー", with: 1
       fill_in "値段", with: 1
       attach_file '画像', "#{Rails.root}/public/lawson/6001.png"
-      binding.irb
-      # click_button "タスクを登録する"
-
+      click_button "タスクを登録する"
+      expect(page).to have_content 'おじさん'
     end
 
     it 'タスク詳細' do
+      visit new_user_session_path
+      fill_in "メール", with: "cclemon3248@gmail.com"
+      fill_in "パスワード", with: "s5mYmhCy"
+      click_button "ログイン"
+      visit tasks_path
+      click_link "見る"
+      expect(page).to have_content 'お肉'
     end
 
-    it 'タスク編集' do
-    end
+     it 'タスク編集' do
+      visit new_user_session_path
+      fill_in "メール", with: "cclemon3248@gmail.com"
+      fill_in "パスワード", with: "s5mYmhCy"
+      click_button "ログイン"
+      visit tasks_path
+      click_link "編集"
+      select "セブンイレブン", from: "task_store"
+      fill_in "商品名", with: "おばさん"
+      fill_in "タンパク質量", with: 10
+      fill_in "カロリー", with: 10
+      fill_in "値段", with: 10
+      attach_file '画像', "#{Rails.root}/public/lawson/6002.png"
+      click_button "タスクを登録する"
+      expect(page).to have_content 'おばさん'
+     end
 
     it 'タスク削除' do
+      visit new_user_session_path
+      fill_in "メール", with: "cclemon3248@gmail.com"
+      fill_in "パスワード", with: "s5mYmhCy"
+      click_button "ログイン"
+      visit tasks_path
+      accept_confirm do
+        click_link "削除"
+      end
+      expect(page).not_to have_content 'お肉'
     end
-
   end
 end
